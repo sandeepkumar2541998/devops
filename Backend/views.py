acb1d83ee8c8518dd49d3e6207d92cd195b08066
Sandeep Kumar <sandeepkumaryadav2541998@gmail.com>
2020-12-24 18:15:25 +0530

    hey

---
+++ Backend/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+def home(request):
+    dests = Destination.objects.all()
+    return render(request,'home.html',{'dests':dests})
